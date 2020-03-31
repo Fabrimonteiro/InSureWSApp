8be647b098e84f1c71ce5390777a47f10529d512
fabrimonteiro <fabicio.monte2000@gmail.com>
2020-03-31 16:11:43 +0100

    Initial commit

---
+++ app/src/main/java/pt/ulisboa/tecnico/sise/insure/app/activities/MainActivity.java
@@ -0,0 +1,28 @@
+package pt.ulisboa.tecnico.sise.insure.app.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import pt.ulisboa.tecnico.sise.insure.app.WSCallTask;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "SISE_MainActivity";
+    private TextView resultView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultView = findViewById(R.id.result);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        resultView.setText("");
+        // create separate AsynkTasks that behave differently for each request in different app
+        new WSCallTask(resultView).execute();
+    }
+}
